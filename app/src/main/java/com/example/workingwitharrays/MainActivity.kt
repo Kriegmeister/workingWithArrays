55e79f1519846bab2fe64e652608378a20324775
Kriegmeister <elishaamon.garma.cics@ust.edu.ph>
2023-10-03 16:56:19 +0800

    Up to MP3 requirements still working on the update button

--- app/src/main/java/com/example/workingwitharrays/MainActivity.kt
+++ app/src/main/java/com/example/workingwitharrays/MainActivity.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import android.widget.Toast
 import java.lang.Exception
 
+data class Account(val username: String, val password: String)
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +24,30 @@ class MainActivity : AppCompatActivity() {
 
         //declaration of edit text variable
         var editUsername = findViewById<EditText>(R.id.edtUsername)
+        var editPassword = findViewById<EditText>(R.id.edtPassword)
 
-        //Declaration of textview variable
+        //Declaration of textview variable  probably no longer used
         var textCounter = findViewById<TextView>(R.id.txtCounter)
 
         //Declaration of array list
-        var arrayAccount = ArrayList<String>()
+        var arrayAccount = ArrayList<Account>()
 
         //buttons
         buttonAdd.setOnClickListener{
          try{
-            if (!editUsername.text.toString().isNullOrEmpty()) {
-                arrayAccount.add(editUsername.text.toString())
-                textCounter.setText(arrayAccount.size.toString())
+             val username = editUsername.text.toString()
+             val password = editPassword.text.toString()
+            if (!username.isNullOrBlank() && !password.isNullOrBlank()) {
+                val newAccount = Account(username, password)
+                arrayAccount.add(newAccount)
+                //textCounter.setText(arrayAccount.size.toString())
                 Log.i("info_garma", "New user is added: ${editUsername.text.toString()}")
                 toastFunc("User Account has been added successfully!")
             }else {
-                Log.e("error_garma", "User Account is null or blank")
-                toastFunc("Kindly insert value in User Account")
+                Log.e("error_garma", "Username and/or Password is null or blank")
+                toastFunc("Kindly insert value in Username AND password")
                 editUsername.setText("")
+                editPassword.setText("")
             }
          }catch(e:Exception){
              Log.e("error_garma", e.message.toString())
@@ -48,15 +55,24 @@ class MainActivity : AppCompatActivity() {
         }
         buttonUpdate.setOnClickListener{
             try {
-                var existingAccount = arrayAccount.indexOf(editUsername.text.toString())
-                if (existingAccount > 0){
-                    arrayAccount[existingAccount] = editUsername.text.toString() + "_updated"
-                    Log.i("info_garma", "New user is updated: ${editUsername.text.toString()}")
-                    toastFunc("User Account has been updated successfully!")
+                val username = editUsername.text.toString()
+                val password = editPassword.text.toString()
+                var existingAccount = arrayAccount.indexOfFirst{it.username == username}
+                if (!username.isNullOrBlank() && !password.isNullOrBlank() && existingAccount >= 0){
+                    val savedPassword = arrayAccount[existingAccount].password
+                    if (password == savedPassword) {
+                        arrayAccount[existingAccount] = Account(username, password)
+                        Log.i("info_garma", "New user is updated: ${editUsername.text.toString()}")
+                        toastFunc("User Account has been updated successfully!")
+                    }else {
+                        Log.e("error_garma", "Mismatch password")
+                        toastFunc("Password is incorrect")
+                    }
                 }else {
                     Log.e("error_garma", "User Account does not exist")
                     toastFunc("Kindly insert value in User Account")
                     editUsername.setText("")
+                    editPassword.setText("")
                 }
             }catch(e:Exception) {
                 Log.e("error_garma", e.message.toString())
@@ -64,15 +80,24 @@ class MainActivity : AppCompatActivity() {
         }
         buttonRemove.setOnClickListener{
             try {
-                var existingAccount = arrayAccount.indexOf(editUsername.text.toString())
-                if (existingAccount >= 0){
-                    Log.i("info_garma", "New user is deleted: ${editUsername.text.toString()}")
-                    arrayAccount.removeAt(existingAccount)
-                    toastFunc("User Account has been deleted successfully!")
+                val username = editUsername.text.toString()
+                val password = editPassword.text.toString()
+                var existingAccount = arrayAccount.indexOfFirst{it.username == username}
+                if (!username.isNullOrBlank() && !password.isNullOrBlank() && existingAccount >= 0){
+                    val savedPassword = arrayAccount[existingAccount].password
+                    if(password == savedPassword) {
+                        Log.i("info_garma", "New user is deleted: ${editUsername.text.toString()}")
+                        arrayAccount.removeAt(existingAccount)
+                        toastFunc("User Account has been deleted successfully!")
+                    }else {
+                        Log.e("error_garma", "Mismatch password")
+                        toastFunc("Password is incorrect")
+                    }
                 }else {
                     Log.e("error_garma", "User Account does not exist")
                     toastFunc("Kindly insert value in User Account")
                     editUsername.setText("")
+                    editPassword.setText("")
                 }
             }catch(e:Exception) {
                 Log.e("error_garma", e.message.toString())
@@ -80,8 +105,9 @@ class MainActivity : AppCompatActivity() {
         }
         buttonShow.setOnClickListener{
          try {
+             Log.e("show_account", "Accounts:")
              for (element in arrayAccount){
-                 Log.e("show_account", "For loop: ${element}")
+                 Log.e("show_account", " ${element}")
              }
          } catch(e:Exception) {
              Log.e("error_garma", e.message.toString())
